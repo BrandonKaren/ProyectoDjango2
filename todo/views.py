420af9bff7fc5d4809a3fba9de1e64959212db7a
juanchorios <101459837+BrandonKaren@users.noreply.github.com>
2023-03-23 20:59:59 -0500

    .

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Tarea
+from .forms import TareaForm
 # Create your views here.
+def home(request):
+    tareas=Tarea.objects.all()
+    context={'tareas':tareas}
+    return render(request,'home.html')
+
+def agregar(request):
+    if request.method=="POST":
+        form= TareaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            form=TareaForm()
+        context={'form': form}
+        return render(request, 'agregar.html', context)
+
+def eliminar(request )
